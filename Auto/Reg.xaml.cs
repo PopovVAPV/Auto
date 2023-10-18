7b73f7fe9f215bd9229438f8914fb917dbd3e822
admin <admin@3205EC08>
2023-10-18 10:54:34 +0300

    Добавьте файлы проекта.

---
+++ Auto/Reg.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Auto
+{
+    /// <summary>
+    /// Логика взаимодействия для Reg.xaml
+    /// </summary>
+    public partial class Reg : Window
+    {
+        public Reg()
+        {
+            InitializeComponent();
+        }
+
+        private void OutClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Yes(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void No(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Yes1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void No1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
